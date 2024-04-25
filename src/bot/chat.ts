b3ae02e5e5a9520f6b60d951a9615b8e17dc6fcd
winstxnhdw <winstxnhdw@gmail.com>
2024-04-26 02:15:49 +0800

    feat: add summariser

--- src/bot/chat.ts
+++ src/bot/chat.ts
@@ -18,8 +18,8 @@ chat.on('message:text', async (context) => {
   context.chatAction = 'typing'
 
   const messages = await context.env.telegroq.get<Message[]>(context.from.username, 'json')
-  const question = { role: 'user', content: context.message.text }
-  const new_messages = messages ? [...messages, question] : [question]
+  const question = { role: 'user', content: context.message.text } as Message
+  let new_messages = messages ? [...messages, question] : [question]
   const groq = new Groq({ apiKey: context.env.GROQ_API_KEY })
   const chat_completion = await groq.chat.completions.create({
     messages: new_messages,
@@ -27,12 +27,24 @@ chat.on('message:text', async (context) => {
   })
 
   const response = chat_completion.choices[0]?.message.content
+  const total_tokens = chat_completion.usage?.total_tokens
 
   if (!response) {
     return context.reply('There was an error with the chat bot!')
   }
 
   new_messages.push({ role: 'assistant', content: response })
+
+  if (total_tokens && total_tokens > 8192) {
+    const context_to_summarise = new_messages
+      .slice(0, -2)
+      .map(({ content }) => content)
+      .join('\n')
+
+    const summary: string = await context.env.ai.run('@cf/facebook/bart-large-cnn', context_to_summarise)
+    new_messages = [{ role: 'system', content: summary }, ...new_messages.slice(-2)]
+  }
+
   await context.env.telegroq.put(context.from.username, JSON.stringify(new_messages))
 
   return context.replyWithHTML(await parseInline(response))
--- src/types/bindings.ts
+++ src/types/bindings.ts
@@ -2,4 +2,6 @@ import type { get_config } from '@/config'
 
 export type Bindings = ReturnType<typeof get_config> & {
   telegroq: KVNamespace
+  // biome-ignore lint/suspicious/noExplicitAny: no type available yet
+  ai: any
 }
