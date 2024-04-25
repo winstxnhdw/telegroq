453b90e55e64faf98e443b10dca1ca709ee7d596
winstxnhdw <winstxnhdw@gmail.com>
2024-04-26 01:34:08 +0800

    feat: store chat history

--- src/bot/chat.ts
+++ src/bot/chat.ts
@@ -6,24 +6,36 @@ import { parseInline } from 'marked'
 
 const chat = new Composer<GrammyContext>()
 
+type Message = {
+  role: 'system' | 'user' | 'assistant'
+  content: string
+}
+
 chat.on('message:text', async (context) => {
   if (!context.from.username) return
   if (await is_not_member(context.env.telegroq, context.from.username)) return
 
   context.chatAction = 'typing'
 
+  const messages = await context.env.telegroq.get<Message[]>(context.from.username, 'json')
+  const question = { role: 'user', content: context.message.text }
+  const new_messages = messages ? [...messages, question] : [question]
   const groq = new Groq({ apiKey: context.env.GROQ_API_KEY })
   const chat_completion = await groq.chat.completions.create({
-    messages: [
-      // { role: 'system', content: 'start' },
-      { role: 'user', content: context.message.text ?? '' },
-    ],
+    messages: new_messages,
     model: 'llama3-70b-8192',
   })
 
-  return context.replyWithHTML(
-    await parseInline(chat_completion.choices[0]?.message.content ?? 'There was an error with the chat bot!'),
-  )
+  const response = chat_completion.choices[0]?.message.content
+
+  if (!response) {
+    return context.reply('There was an error with the chat bot!')
+  }
+
+  new_messages.push({ role: 'assistant', content: response })
+  await context.env.telegroq.put(context.from.username, JSON.stringify(new_messages))
+
+  return context.replyWithHTML(await parseInline(response))
 })
 
 export { chat }
