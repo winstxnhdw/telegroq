b18922484385658beed8b7676c944f8b7f50a3ac
winstxnhdw <winstxnhdw@gmail.com>
2024-06-27 03:49:09 +0800

    style: use braces after anonymous function

--- src/bot/composers/start.ts
+++ src/bot/composers/start.ts
@@ -3,6 +3,8 @@ import { Composer } from 'grammy'
 
 const start = new Composer<GrammyContext>()
 
-start.command('start', (context) => context.kv.put_user_id(context.member.username, context.member.id))
+start.command('start', (context) => {
+  return context.kv.put_user_id(context.member.username, context.member.id)
+})
 
 export { start }
