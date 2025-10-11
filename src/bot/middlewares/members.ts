471f6c56d83969a7651c8d1902b939379d1801bd
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-10-12 06:17:33 +0800

    feat: temporarily allow non-members
    Commented out the check for non-members in the middleware.

--- src/bot/middlewares/members.ts
+++ src/bot/middlewares/members.ts
@@ -9,7 +9,7 @@ const is_not_member = async (kv: KVNamespace, username: string): Promise<boolean
 
 export const members = (environment: Bindings) => async (context: Context & Member, next: NextFunction) => {
   if (!context.from || !context.from.username) return
-  if (await is_not_member(environment.telegroq, context.from.username)) return
+  // if (await is_not_member(environment.telegroq, context.from.username)) return
 
   context.member = {
     username: context.from.username,
