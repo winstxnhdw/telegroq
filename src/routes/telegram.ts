89470320dc0a1b297d435af21d4585021404d850
winstxnhdw <winstxnhdw@gmail.com>
2025-10-12 10:40:33 +0800

    fix: correctly parse origin

--- src/routes/telegram.ts
+++ src/routes/telegram.ts
@@ -4,6 +4,6 @@ import { bot_factory } from '@/bot'
 import type { HonoContext } from '@/types'
 
 export const telegram = new Hono<HonoContext>().post('/telegram', (context) => {
-  context.env.BOT_URL = context.req.url
+  context.env.BOT_URL = new URL(context.req.url).origin
   return webhookCallback(bot_factory(context.env), 'hono')(context)
 })
